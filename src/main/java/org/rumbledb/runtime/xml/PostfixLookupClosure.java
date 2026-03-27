ed928d447e30d571823c2a918c6014dfe02e728b
EPMatt <30753195+EPMatt@users.noreply.github.com>
2026-03-27 12:58:12 +0000

    fix: update lookup iterators and closures to match spec

--- src/main/java/org/rumbledb/runtime/xml/PostfixLookupClosure.java
+++ src/main/java/org/rumbledb/runtime/xml/PostfixLookupClosure.java
@@ -43,16 +43,43 @@ public class PostfixLookupClosure implements FlatMapFunction<Item, Item> {
     public Iterator<Item> call(Item arg0) throws Exception {
         List<Item> results = new ArrayList<>();
 
-        if (this.wildcard) {
-            if (arg0.isObject()) {
-                if (!arg0.allowsNonSingletons()) {
-                    results = arg0.getValues();
+
+        if (arg0.isMap()) {
+            if (this.wildcard) {
+                if (arg0.isObject()) {
+                    results.addAll(arg0.getItemValues());
                 } else {
-                    for (java.util.List<Item> valueSequence : arg0.getSequences()) {
+                    for (List<Item> valueSequence : arg0.getSequenceValues()) {
                         results.addAll(valueSequence);
                     }
                 }
-            } else if (arg0.isArray()) {
+            } else {
+                for (Item rawKey : this.keys) {
+                    // Align with map:get and FO lookup semantics: atomize and require exactly one atomic key.
+                    List<Item> atomized = rawKey.atomizedValue();
+                    if (atomized.size() != 1 || !atomized.get(0).isAtomic()) {
+                        throw new UnexpectedTypeException(
+                                "Map lookup key must atomize to a single atomic value [err:XPTY0004].",
+                                ExceptionMetadata.EMPTY_METADATA
+                        );
+                    }
+                    Item key = atomized.get(0);
+                    if (arg0.isObject()) {
+                        // fast path: one item per key
+                        Item value = arg0.getItemByKey(key);
+                        if (value != null) {
+                            results.add(value);
+                        }
+                    } else {
+                        List<Item> valueSequence = arg0.getSequenceByKey(key);
+                        if (valueSequence != null && !valueSequence.isEmpty()) {
+                            results.addAll(valueSequence);
+                        }
+                    }
+                }
+            }
+        } else if (arg0.isArray()) {
+            if (this.wildcard) {
                 if (!arg0.allowsNonSingletons()) {
                     results = arg0.getItems();
                 } else {
@@ -60,40 +87,26 @@ public class PostfixLookupClosure implements FlatMapFunction<Item, Item> {
                         results.addAll(member);
                     }
                 }
-            }
-            return results.iterator();
-        }
-        if (arg0.isObject()) {
-            for (Item key : this.keys) {
-                if (!arg0.allowsNonSingletons()) {
-                    Item i = arg0.getItemByKey(key);
-                    if (i != null) {
-                        results.add(i);
+            } else {
+                for (Item key : this.keys) {
+                    if (key.isString()) {
+                        throw new UnexpectedTypeException(
+                                "Type error; Lookup with String on Arrays is not possible",
+                                ExceptionMetadata.EMPTY_METADATA
+                        );
                     }
-                } else {
-                    List<Item> sequence = arg0.getSequenceByKey(key);
-                    if (sequence != null) {
-                        results.addAll(sequence);
-                    }
-                }
-            }
-        } else if (arg0.isArray()) {
-            for (Item key : this.keys) {
-                if (key.isString()) {
-                    throw new UnexpectedTypeException(
-                            "Type error; Lookup with String on Arrays is not possible",
-                            ExceptionMetadata.EMPTY_METADATA
-                    );
-                }
-                if (key.isNumeric()) {
-                    int idx = key.castToIntValue() - 1;
-                    if (!arg0.allowsNonSingletons()) {
-                        results.add(arg0.getItemAt(idx));
-                    } else {
-                        results.addAll(arg0.getSequenceAt(idx));
+                    if (key.isNumeric()) {
+                        int idx = key.castToIntValue() - 1;
+                        if (!arg0.allowsNonSingletons()) {
+                            results.add(arg0.getItemAt(idx));
+                        } else {
+                            results.addAll(arg0.getSequenceAt(idx));
+                        }
                     }
                 }
+
             }
+
         }
         return results.iterator();
     }
